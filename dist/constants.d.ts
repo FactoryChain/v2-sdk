a7389bab643d7e37dcdcad9feb3c20a875212de4
FAC <fac@gmail.com>
2022-06-24 18:53:26 +0700

    init

---
+++ dist/constants.d.ts
@@ -0,0 +1,9 @@
+import JSBI from 'jsbi';
+export declare const FACTORY_ADDRESS = "0xcdb5aB5875910BdE9c955eB46270FfF8e9D9fB61";
+export declare const INIT_CODE_HASH = "0xe4c189abba493bcfaa8c55c287d7a6ed247c83a31f76221097a551a7c642c5c0";
+export declare const MINIMUM_LIQUIDITY: JSBI;
+export declare const ZERO: JSBI;
+export declare const ONE: JSBI;
+export declare const FIVE: JSBI;
+export declare const _997: JSBI;
+export declare const _1000: JSBI;
---
+++ src/constants.ts
@@ -0,0 +1,14 @@
+import JSBI from 'jsbi'
+
+export const FACTORY_ADDRESS = '0xcdb5aB5875910BdE9c955eB46270FfF8e9D9fB61'
+
+export const INIT_CODE_HASH = '0xe4c189abba493bcfaa8c55c287d7a6ed247c83a31f76221097a551a7c642c5c0'
+
+export const MINIMUM_LIQUIDITY = JSBI.BigInt(1000)
+
+// exports for internal consumption
+export const ZERO = JSBI.BigInt(0)
+export const ONE = JSBI.BigInt(1)
+export const FIVE = JSBI.BigInt(5)
+export const _997 = JSBI.BigInt(997)
+export const _1000 = JSBI.BigInt(1000)
